020d9b75672358bc94cd854766338b44e169f23f
Muzammil-hasan <muzammilsyed270300@gmail.com>
2021-03-09 16:23:21 +0530

    few bug fixed && cursor changed

--- src/js/script.js
+++ src/js/script.js
@@ -1,109 +1,123 @@
 /* ##################################### */
-/* Splitting Text */
+/*           Splitting Text              */
 /* ##################################### */
 
 Splitting();
 
 /* ##################################### */
-/* Menu Animation */
+/*           Menu Animation              */
 /* ##################################### */
 
 const html = document.documentElement;
+
 const overlay = document.querySelector(".overlay");
+
 let hamburger = document.querySelector(".hamburger");
+
+let cta = document.querySelector(".cta-wrapper a");
+
+let chars = document.querySelectorAll(".cta-wrapper .char");
+
 const overlayWidth = overlay.clientWidth;
+
 let menuTl = gsap.timeline();
+
 menuTl.reversed(true);
+
 let expanded = false;
 
 /* Overlay size */
 
 const getVpdr = () => {
-  const vph = Math.pow(html.offsetHeight, 2); // Height
-  const vpw = Math.pow(html.offsetWidth, 2); // Width
-  const vpd = Math.sqrt(vph + vpw); // Diagonal
-  return (vpd * 3.5) / overlayWidth; // Circle radius
+	const vph = Math.pow(html.offsetHeight, 2); // Height
+
+	const vpw = Math.pow(html.offsetWidth, 2); // Width
+
+	const vpd = Math.sqrt(vph + vpw); // Diagonal
+
+	return (vpd * 3.5) / overlayWidth; // Circle radius
 };
 
 menuTl
-  .to(".overlay", {
-    duration: 1.5,
-    scale: getVpdr(),
+	.to(".overlay", {
+		duration: 1.5,
+		scale: getVpdr(),
 
-    ease: Power3.easeInOut,
-  })
-  .to(
-    ".logo-path",
-    {
-      color: "#171717",
-    },
-    "-=1"
-  )
-  .to(
-    ".main-nav",
-    {
-      display: "block",
-    },
-    "-=1"
-  )
-  .to(
-    ".social-links",
-    {
-      display: "block",
-    },
-    "-=.8"
-  )
-  .to(
-    ".menu-indicator",
-    {
-      display: "block",
-    },
-    "-=.8"
-  )
-  .from(
-    ".main-nav ul li",
-    {
-      x: -100,
-      opacity: 0,
-      stagger: 0.1,
-      ease: Power2.easeOut,
-    },
-    "-=1"
-  )
-  .from(
-    ".social-links ul li",
-    {
-      yPercent: 100,
-      opacity: 0,
-      stagger: 0.1,
-      ease: Power2.easeOut,
-    },
-    "-=1"
-  );
+		ease: Power3.easeInOut,
+	})
+	.to(
+		".logo-wrapper a svg",
+		{
+			color: "#171717",
+		},
+		"-=1"
+	)
+	.to(
+		".main-nav",
+		{
+			display: "block",
+		},
+		"-=1"
+	)
+	.to(
+		".social-links",
+		{
+			display: "block",
+		},
+		"-=.8"
+	)
+	.to(
+		".menu-indicator",
+		{
+			display: "block",
+		},
+		"-=.8"
+	)
+	.from(
+		".main-nav ul li",
+		{
+			x: -100,
+			opacity: 0,
+			stagger: 0.1,
+			ease: Power2.easeOut,
+		},
+		"-=1"
+	)
+	.from(
+		".social-links ul li",
+		{
+			yPercent: 100,
+			opacity: 0,
+			stagger: 0.1,
+			ease: Power2.easeOut,
+		},
+		"-=1"
+	);
 
 function overlayExpand() {
-  menuTl.reversed(!menuTl.reversed());
+	menuTl.reversed(!menuTl.reversed());
 }
 
 hamburger.addEventListener("click", () => {
-  expanded = !expanded;
-  hamburger.classList.toggle("is-active");
-  overlayExpand();
-  let cta = document.querySelector(".cta-wrapper a");
-  let chars = document.querySelectorAll(".char");
-  let logo = document.querySelector(".logo-wrapper a svg");
-  for (const char of chars) {
-    char.classList.toggle("is-black");
-  }
-  cta.classList.toggle("black");
-  cursor.classList.toggle("bg-black");
+	overlayExpand();
+
+	expanded = !expanded;
+
+	hamburger.classList.toggle("is-active");
+
+	for (const char of chars) {
+		if (hamburger.classList.contains("is-active")) {
+			char.classList.add("is-black");
+
+			cta.classList.add("black");
+		} else if (!hamburger.classList.contains("is-active")) {
+			char.classList.remove("is-black");
+
+			cta.classList.remove("black");
+		}
+	}
 });
-hamburger.addEventListener("mouseover", () => {
-  cursor.classList.add("expand");
-});
-hamburger.addEventListener("mouseleave", () => {
-  cursor.classList.remove("expand");
-});
+
 /* Overlay size if Resized */
 
 // if (hamburger.classList.contains("is-active")) {
@@ -116,139 +130,276 @@ hamburger.addEventListener("mouseleave", () => {
 //   };
 // } else {
 // }
+
 /* ##################################### */
-/* Cursor Animation && Global */
+/*          Cursor Animation             */
 /* ##################################### */
-let scrollIndicator = document.querySelectorAll(".scroll .char");
+
 const cursor = document.querySelector(".cursor");
+
+const follower = document.querySelector(".cursor-follower");
+
 const links = document.querySelectorAll("a");
-for (const link of links) {
-  link.addEventListener("mousemove", () => {
-    cursor.classList.add("expand");
-  });
-  link.addEventListener("mouseleave", () => {
-    cursor.classList.remove("expand");
-  });
+const allLinks = [...links, hamburger];
+
+var posX = 0,
+	posY = 0,
+	mouseX = 0,
+	mouseY = 0;
+
+TweenMax.to({}, 0.016, {
+	repeat: -1,
+	onRepeat: function () {
+		posX += (mouseX - posX) / 9;
+		posY += (mouseY - posY) / 9;
+
+		TweenMax.set(follower, {
+			css: {
+				left: posX - 20,
+				top: posY - 20,
+			},
+		});
+
+		TweenMax.set(cursor, {
+			css: {
+				left: mouseX,
+				top: mouseY,
+			},
+		});
+	},
+});
+
+for (const link of allLinks) {
+	link.addEventListener("mousemove", () => {
+		cursor.classList.add("active");
+		follower.classList.add("active");
+	});
+
+	link.addEventListener("mouseleave", () => {
+		cursor.classList.remove("active");
+		follower.classList.remove("active");
+	});
 }
 
-document.addEventListener("mousemove", (e) => {
-  cursor.setAttribute(
-    "style",
-    "top: " + (e.pageY - 20) + "px; left: " + (e.pageX - 20) + "px;"
-  );
+document.addEventListener("mousemove", function (e) {
+	mouseX = e.pageX;
+	mouseY = e.pageY;
 });
 
-gsap.from(scrollIndicator, {
-  xPercent: -60,
-  duration: 1,
-  delay: 2,
-  opacity: 0,
-  ease: "elastic.out(1, 0.5)",
-  stagger: 0.03,
-});
 /* ##################################### */
-/* Header Animations */
+/*          Global ANimation             */
+/* ##################################### */
+
+gsap.from(".scroll .char", {
+	xPercent: -60,
+	duration: 1,
+	delay: 2,
+	opacity: 0,
+	ease: "elastic.out(1, 0.5)",
+	stagger: 0.03,
+});
+
+/* ##################################### */
+/*           Header Animations           */
 /* ##################################### */
 
 gsap.from(".header", {
-  yPercent: -250,
-  duration: 2,
-  delay: 1,
-  stagger: 0.5,
-  ease: Expo.easeOut,
-  opacity: 0,
+	yPercent: -250,
+	duration: 2,
+	delay: 1,
+	stagger: 0.5,
+	ease: Expo.easeOut,
+	opacity: 0,
 });
 
 /* ##################################### */
-/* Hero Section Animations */
+/*       Hero Section Animations         */
 /* ##################################### */
 
 let heroAnim = new gsap.timeline();
+
 let nameChars = document.querySelectorAll(".name .char");
+
 let titleChars = document.querySelectorAll(".title .word");
+
 let ellipse = document.querySelector(".ellipse");
-let cta = document.querySelector(".cta a");
-let ctaElements = [cta, ellipse];
+
+let heroCta = document.querySelector(".cta a");
+
+let ctaElements = [heroCta, ellipse];
+
 heroAnim
-  .from(".hero", {
-    xPercent: -500,
-    duration: 1.5,
-    ease: Power1.out,
-    opacity: 0,
-    delay: 0.5,
-  })
-  .from(nameChars, {
-    xPercent: -60,
-    duration: 1,
-    opacity: 0,
-    ease: "elastic.out(1, 0.5)",
-    stagger: 0.05,
-  })
-  .from(
-    ".line span",
-    {
-      y: 400,
-      rotate: 30,
-      duration: 1.5,
-      ease: "power4.out",
-      skewY: 10,
-      stagger: 0.2,
-    },
-    "-=1.5"
-  )
-  .from(
-    ".subtitle",
-    {
-      yPercent: 60,
-      duration: 0.5,
-      opacity: 0,
-      ease: Power2.easeOut,
-    },
-    "-=1"
-  )
-  .from(
-    ".ellipse",
-    {
-      xPercent: -50,
-      opacity: 0,
-      scale: 0,
-      duration: 1,
-      ease: Power2.easeIn,
-      transformOrigin: "center",
-    },
-    "-=1.5"
-  )
-  .from(
-    ".cta a",
-    {
-      xPercent: -50,
-      opacity: 0,
-      duration: 1,
-      ease: Power2.easeIn,
-      transformOrigin: "center",
-    },
-    "-=1.2"
-  );
+	.set(".hero", {
+		xPercent: -500,
+	})
+	.to(".hero", {
+		xPercent: 0,
+		duration: 1.5,
+		ease: Power1.out,
+		opacity: 1,
+		delay: 0.5,
+	})
+	.from(nameChars, {
+		xPercent: -60,
+		duration: 1,
+		opacity: 0,
+		ease: "elastic.out(1, 0.5)",
+		stagger: 0.05,
+	})
+	.from(
+		".line span",
+		{
+			y: 600,
+			skewY: 100,
+			duration: 1.5,
+			ease: "power4.out",
+			skewY: 10,
+			stagger: 0.2,
+		},
+		"-=1.5"
+	)
+	.from(
+		".subtitle",
+		{
+			yPercent: 60,
+			duration: 0.5,
+			opacity: 0,
+			ease: Power2.easeOut,
+		},
+		"-=1"
+	)
+	.from(
+		".ellipse",
+		{
+			xPercent: -50,
+			opacity: 0,
+			scale: 0,
+			duration: 1,
+			ease: Power2.easeIn,
+			transformOrigin: "center",
+		},
+		"-=1.5"
+	)
+	.from(
+		".cta a",
+		{
+			xPercent: -50,
+			opacity: 0,
+			duration: 1,
+			ease: Power2.easeIn,
+			transformOrigin: "center",
+		},
+		"-=1.2"
+	);
 
-ctaElements.forEach((i) => {
-  i.addEventListener("mouseover", () => {
-    gsap.to(ellipse, {
-      width: "14.5rem",
-      duration: 0.05,
-      ease: Power1.easeIn,
-    });
-  });
+ctaElements.forEach((element) => {
+	element.addEventListener("mouseover", () => {
+		gsap.to(ellipse, {
+			width: "14.5rem",
+			duration: 0.05,
+			ease: Power1.easeIn,
+		});
+	});
 });
-ctaElements.forEach((i) => {
-  i.addEventListener("mouseleave", () => {
-    gsap.to(ellipse, {
-      width: "4rem",
-      duration: 0.05,
-      ease: Power1.easeIn,
-    });
-  });
+ctaElements.forEach((element) => {
+	element.addEventListener("mouseleave", () => {
+		gsap.to(ellipse, {
+			width: "4rem",
+			duration: 0.05,
+			ease: Power1.easeIn,
+		});
+	});
 });
 
 /* ##################################### */
-/* Projects Section Animations */
+/*      Projects Section Animations      */
 /* ##################################### */
+
+let lines = document.querySelectorAll(".hamburger .line");
+
+let logoSVG = document.querySelector(".logo-wrapper a svg");
+
+let scrollChars = document.querySelectorAll(".scroll .char");
+
+let scrollIndicator = document.querySelector(".scroll");
+
+let projectAnim = new TimelineMax({
+	scrollTrigger: {
+		trigger: ".projects",
+		start: "top 10%",
+		onEnter: () => {
+			classForLightBg();
+		},
+		onLeave: () => {
+			classForDarktBg();
+		},
+		onEnterBack: () => {
+			classForLightBg();
+		},
+		onLeaveBack: () => {
+			classForDarktBg();
+		},
+		// markers: true,
+	},
+});
+
+gsap.to(".item img", {
+	scrollTrigger: {
+		trigger: ".item",
+		start: "top bottom",
+		// markers: true,
+	},
+	translateY: "0%",
+	opacity: 1,
+	scaleY: 1,
+	duration: 0.05,
+	ease: Expo.ease,
+});
+
+function classForLightBg() {
+	for (const char of chars) {
+		char.classList.add("is-black");
+	}
+
+	hamburger.classList.add("bg-black");
+
+	cta.classList.add("black");
+
+	for (const line of lines) {
+		line.classList.add("bg-white");
+	}
+
+	logoSVG.classList.add("color-black");
+
+	for (const scrollChar of scrollChars) {
+		scrollChar.classList.add("is-black");
+	}
+}
+function classForDarktBg() {
+	hamburger.classList.remove("bg-black");
+
+	cta.classList.remove("black");
+
+	for (const line of lines) {
+		line.classList.remove("bg-white");
+	}
+
+	for (const char of chars) {
+		char.classList.remove("is-black");
+	}
+
+	logoSVG.classList.remove("color-black");
+
+	for (const scrollChar of scrollChars) {
+		scrollChar.classList.remove("is-black");
+	}
+}
+
+/* ##################################### */
+/*               Tilt Js                 */
+/* ##################################### */
+
+VanillaTilt.init(document.querySelectorAll(".item"), {
+	max: 25,
+	speed: 400,
+});
